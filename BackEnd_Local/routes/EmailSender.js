fbc7da068078952b355e77905215725f92f5d38f
KatonaZs <63052470+KatonaZs@users.noreply.github.com>
2021-03-07 18:02:57 +0100

    Change Email Sending Component to local version.

--- BackEnd_Local/routes/EmailSender.js
+++ BackEnd_Local/routes/EmailSender.js
@@ -1,14 +1,21 @@
 const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
 
-module.exports = async function SendMail(mail,recepient,subject){
-let transporter = nodemailer.createTransport({host: 'mx.inf.unideb.hu',port:25,secure:false});
-const message = {  from: 'Doktori Iskola <noreply@inf.unideb.hu>', // sender address
-                   to: recepient, // list of receivers
-                   subject: subject, // Subject line
-                   html: mail, // html body
-                }
-await transporter.sendMail(message,function(err,info) {
-  if(err)
-    console.log(err)
-})
+
+module.exports = async function SendMail(mail,recepient){
+
+const auth = {
+        auth: {
+            api_key: process.env.API_KEY || 'mailgun_api_key', // TODO: 
+            domain: process.env.DOMAIN || 'mailgun_domain' // TODO:
+        }};
+    
+let transporter = nodemailer.createTransport( mailGun(auth) );
+
+await transporter.sendMail({
+                from: 'Doktori Iskola <noreply@unideb.hu>', // sender address
+                to: recepient, // list of receivers
+                subject: "Registration Data", // Subject line
+                html: mail, // html body
+              });
 }
